90cd415d8b9e1c654bacb4cd40fc5cddbdbe4e3d
Luis Santiago <luis.santiagoayala@gmail.com>
2016-10-12 22:36:54 -0700

    Add environment port to variable

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const random = require('./randomNumberGenerator') 
 const app = express()  
-const port = 3000
+const port = Number(process.env.PORT || 3000);
 
 app.use((request, response, next) => {  
   console.log(request.headers)
