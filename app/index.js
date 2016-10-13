3e6d622fcc146d65430368680e8ca74a5c0b6eb0
Luis Santiago <lsantiago@opentable.com>
2016-10-13 16:49:03 -0700

    Add database transaction to read movements

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express') 
-const random = require('./randomNumberGenerator') 
+const random = require('./helpers/randomNumberGenerator')
+const con = require('./config/database').con
 const app = express()  
 const port = Number(process.env.PORT || 3000);
 
@@ -10,10 +11,18 @@ app.use((request, response, next) => {
 
 app.use((request, response, next) => {  
   response.rounds = random.getRandomInt(3, 6)
+  response.movements = random.getRandomInt(3, 6)
   next()
 })
 
-app.get('/api/v1/wodx', (request, response) => {  
+app.get('/api/v1/wod', (request, response) => {  
+  con.query('SELECT movement FROM wodx.movement AS r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM wodx.movement)) AS id) AS r2 WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT '+response.movements,function(err,rows){
+    if(err) throw err;
+
+    console.log('Data received from db:\n');
+    console.log(rows);
+  });
+
   response.json({
   	rounds: response.rounds,
   	movements: {},
